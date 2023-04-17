e7e052e22ab325b41100756b571e44a175019de2
aluno <aluno@email.com>
2023-04-17 08:49:16 -0300

    Criação Activity activity_user_add.xml

---
+++ app/src/main/java/br/edu/ifsp/dmo/app11_login/View/UserAddActivity.java
@@ -0,0 +1,16 @@
+package br.edu.ifsp.dmo.app11_login.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.edu.ifsp.dmo.app11_login.R;
+
+public class UserAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_add);
+    }
+}
